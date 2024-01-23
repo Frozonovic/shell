b496229bc3fbb769adcd00afe8652e983e4a772c
glacialfrozone <blee20@georgefox.edu>
2024-01-22 16:28:26 -0800

    This might actually be finished. Thank you for the brief assistance this morning. :)

--- shell.c
+++ shell.c
@@ -5,10 +5,12 @@
  * A command-line shell program that uses system calls to directly invoke
  * operating system services, rather than relying on C library calls.
  *
- * @author Brian R. Snider (bsnider@georgefox.edu)
+ * @author blee20@georgefox.edu
  */
 
 #include <sys/syscall.h>
+#include <unistd.h>
+#include <string.h>
 #include "shell.h"
 
 /**
@@ -16,8 +18,28 @@
  *
  * @return The exit status of the shell.
  */
-int main() {
-    // TODO implement shell
+char main() {
+    // Good ol' constants
+    const int SUCCESS = 0;
+    const int CHAR_LIMIT = 255;
+    const char* EXIT = "exit\n";
 
-    return 0;
+    // 255 characters is the input limit
+    char buffer[CHAR_LIMIT];
+
+    while (strncasecmp(buffer, EXIT, strlen(EXIT)) != 0) {
+        // Clear the buffer
+        memset(buffer, 0, sizeof(buffer));
+
+        // Prompt user for input via print to standard output
+        syscall(SYS_write, STDOUT_FILENO, "shell$", 6);
+
+        // Read user input from standard output and store in buffer
+        syscall(SYS_read, STDOUT_FILENO, buffer, CHAR_LIMIT);
+
+        // Called in case of sync issues
+        syscall(SYS_fsync, STDOUT_FILENO);
+    }
+
+    return SUCCESS;
 }
